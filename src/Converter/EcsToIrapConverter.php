73d7e62203fa901fce23352face312e925ee8e6d
Petrics Lajos <petrics.lajos@mhk.hu>
2022-07-14 11:47:11 +0200

    két pont közötti távolság felosztással képzett pontok új algoritmusa

--- src/Converter/EcsToIrapConverter.php
+++ src/Converter/EcsToIrapConverter.php
@@ -120,9 +120,13 @@ class EcsToIrapConverter
                 {
                     // $splitLength = floor( $length / ceil( $length / $this->segmentLength )) / 2;
                     // echo "Need to interpolate {$length}  {$this->segmentLength}\n";
-                    $newPoints = $this->GeoUtils->splitArc( floatval( $prevRecord->getFieldValue("latitude")), floatval( $prevRecord->getFieldValue("longitude")),
-                                                            floatval( $TempRecord->getFieldValue("latitude")), floatval( $TempRecord->getFieldValue("longitude")),
-                                                            $this->segmentLength );
+                    //$newPoints = $this->GeoUtils->splitArc( floatval( $prevRecord->getFieldValue("latitude")), floatval( $prevRecord->getFieldValue("longitude")),
+                    //                                        floatval( $TempRecord->getFieldValue("latitude")), floatval( $TempRecord->getFieldValue("longitude")),
+                    //                                        $this->segmentLength );
+                    // teszt new split method
+                    $newPoints = $this->GeoUtils->getIntermediatePoints( floatval( $prevRecord->getFieldValue("latitude")), floatval( $prevRecord->getFieldValue("longitude")),
+                                                                        floatval( $TempRecord->getFieldValue("latitude")), floatval( $TempRecord->getFieldValue("longitude")),
+                                                                        $this->segmentLength );
 
                     foreach ($newPoints as $newPoint )
                     {
